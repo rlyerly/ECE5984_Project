b1cf98e1c2a310a19c463f7ab444e902a0700ac6
Pacifist117 <bigeyedmonster@gmail.com>
2013-02-27 16:39:48 -0500

    I tweaked my plot_all.sh to finish spelling issues in file names. Also finished my analysis of my graphs. As the report is due tomorrow at midnight, I won't be toching it until after class.
    -Kevin

--- plot_all.sh
+++ plot_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #add them as they finish
-for alg in blake256 blake32 blake512 blake64 cubehash816 groestl256 groestl512 jh224 jh256 jh384 jh512 keccak keccakc1024 keccakc256 keccakc418 keccakc512 keccakc768
+for alg in blake256 blake32 blake512 blake64 cubehash816 grostel256 grostel512 jh224 jh256 jh384 jh512 keccak keccakc1024 keccakc256 keccakc448 keccakc512 keccakc768
 do
-    gnuplot -e "set xlabel \"Length of Hash\" ; set ylabel \"Number of Cycles\" ; set terminal png; set output \"${alg}.png\" ; set xrange [0:1024]; plot \"<sed '1,2d' raw_data/${alg}.raw.cycles | awk -f script.awk\" u 1 w l t \"${alg}\"; "
+    gnuplot -e "set xlabel \"Length of Hash\" ; set ylabel \"Number of Cycles\" ; set terminal png; set output \"images/${alg}.png\" ; set xrange [0:1024]; plot \"<sed '1,2d' raw_data/${alg}.raw.cycles | awk -f script.awk\" u 1 w l t \"${alg}\";"
 done
