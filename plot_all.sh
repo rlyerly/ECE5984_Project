a9a3f138d3490bfa7cd370c5e38c20ebaf2e775f
Rob Lyerly <rlyerly@vt.edu>
2013-02-28 07:07:03 -0500

    Added my plots to "plot_all.sh"

--- plot_all.sh
+++ plot_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #add them as they finish
-for alg in blake256 blake32 blake512 blake64 cubehash816 grostel256 grostel512 jh224 jh256 jh384 jh512 keccak keccakc1024 keccakc256 keccakc448 keccakc512 keccakc768
+for alg in blake256 blake32 blake512 blake64 cubehash816 grostel256 grostel512 jh224 jh256 jh384 jh512 keccak keccakc1024 keccakc256 keccakc448 keccakc512 keccakc768 keccakc1024 mgrostl256 sha256 sha512 skein10241024 skein256256 skein512256 skein512512
 do
     gnuplot -e "set xlabel \"Length of Hash\" ; set ylabel \"Number of Cycles\" ; set terminal png; set output \"images/${alg}.png\" ; set xrange [0:1024]; plot \"<sed '1,2d' raw_data/${alg}.raw.cycles | awk -f trimmed_mean.awk\" u 1 w l t \"${alg}\";"
 done
