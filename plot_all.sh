c2b7d8037b124891fa27a4fa60558994ec191390
Pacifist117 <bigeyedmonster@gmail.com>
2013-02-28 16:20:05 -0500

    created fast_run_images, along with my changes to phase1_report
    -Kevin

--- plot_all.sh
+++ plot_all.sh
@@ -8,5 +8,5 @@ ALGOs=('blake256' 'blake32' 'blake512' 'blake64' 'cubehash816' 'groestl256'
 
 for alg in ${ALGOs[@]}
 do
-    gnuplot -e "set xlabel \"Length of Hash\" ; set ylabel \"Number of Cycles\" ; set terminal png; set output \"images/${alg}.png\" ; set xrange [0:1024]; plot \"<sed '1,2d' raw_data/${alg}.raw.cycles | awk -f trimmed_mean.awk\" u 1 w l t \"${alg}\";"
+    gnuplot -e "set xlabel \"Length of Hash\" ; set ylabel \"Number of Cycles\" ; set terminal png; set output \"images_fast_run/${alg}.png\" ; set xrange [0:1024]; plot \"<sed '1,2d' fast_run_data/${alg}.raw.cycles | awk -f trimmed_mean.awk\" u 1 w l t \"${alg}\";"
 done
