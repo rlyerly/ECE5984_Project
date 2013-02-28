63654e8fd7cb34e5d6e604526bae6db27defe7c1
Reese Moore <ram@vt.edu>
2013-02-27 21:23:27 -0500

    Update some images
    Note that this does change the name of some of the data files that I had messed
    up earlier.

--- plot_all.sh
+++ plot_all.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+ALGOs=('blake256' 'blake32' 'blake512' 'blake64' 'cubehash816' 'groestl256'
+       'groestl512' 'jh224' 'jh256' 'jh384' 'jh512' 'keccak' 'keccakc1024'
+       'keccakc256' 'keccakc448' 'keccakc512' 'keccakc768' 'md5' 'mgrostl256'
+       'sha256' 'sha512' 'skein10241024' 'skein256256' 'skein512256'
+       'skein512512')
+
 #add them as they finish
-for alg in blake256 blake32 blake512 blake64 cubehash816 grostel256 grostel512 jh224 jh256 jh384 jh512 keccak keccakc1024 keccakc256 keccakc448 keccakc512 keccakc768
+for alg in ${ALGOs[@]}
 do
     gnuplot -e "set xlabel \"Length of Hash\" ; set ylabel \"Number of Cycles\" ; set terminal png; set output \"images/${alg}.png\" ; set xrange [0:1024]; plot \"<sed '1,2d' raw_data/${alg}.raw.cycles | awk -f trimmed_mean.awk\" u 1 w l t \"${alg}\";"
 done
