9db8080152b8e93878c3c59c9887340698799206
Rob Lyerly <rlyerly@vt.edu>
2013-03-27 11:02:10 -0400

    Added new directorie for phase3 in addition to starting my work on post-processing.

---
+++ phase3/post_processing/post_processing.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include "post_processing.h"
+
+#define DEBUG 0
+
+#define ERR( message ) fprintf(stderr, "Error: message!\n")
+
+int process(pp_data* data)
+{
+	//Preliminary checks, make sure the user didn't do anything stupid...
+	if(data->input_size > 128)
+	{
+		ERR("Input size too large");
+		return FAILURE;
+	}
+
+	if(!data->input_data.sample)
+	{
+		ERR("No input data");
+		return FAILURE;
+	}
+
+	if(!data->output_data)
+	{
+		ERR("No output data buffer");
+		return FAILURE;
+	}
+
+	//Perform the requested type of post-processing
+	switch(data->type)
+	{
+	case NONE:
+		//Copy input data to output data, set output size
+		memcpy(data->output_data, data->input_data.sample,
+				data->input_size);
+		data->output_size = data->input_size;
+		break;
+	case VON_NEUMANN:
+		break;
+	case SHA3:
+		break;
+	case MULTI_SAMPLE:
+		break;
+	case TOEPLITZ:
+		break;
+	default:
+		ERR("Requested unknown type of post-processing");
+		return FAILURE;
+	}
+
+	return SUCCESS;
+}
---
+++ phase3/post_processing/post_processing.h
@@ -0,0 +1,82 @@
+/*
+ * Post-processing Library
+ *
+ * Defines the interface to apply different post-processing techniques to data
+ * obtained from random-number sources to eliminate bias and ensure uniformity.
+ *
+ * Author: Rob Lyerly <rlyerly@vt.edu>
+ */
+#define SUCCESS 0
+#define FAILURE 1
+
+/*
+ * Enumerates the possible types of post-processing available to the user.
+ *
+ * NONE:	Apply no post-processing.  You probably don't need to use this
+ *		library if you're not dispersing the data.  In this case,
+ *		input data is copied directly into the output buffer and the
+ *		input size is returned.
+ *
+ * VON_NEUMANN:	Apply von-Neumann whitening.  This is a stream-processing
+ *		approach where two consecutive bits are analyzed.  If they are
+ *		the same, they are thrown out.  If the first bit is 0 and the
+ *		second bit is 1, then a 1 is output.  If the first bit is 1
+ *		and the second bit is 0, then a 0 is output.  In this way, you
+ *		can get up to 4 bits of randomness per byte.
+ *
+ * SHA3:	Apply the SHA-3 algorithm.  This is a well known standard and
+ *		has good statistical random number properties.  The number of
+ *		post-processed bytes returned is equal to the input size.
+ *
+ * MULTI_SAMPLE:Apply the technique proposed in "Extracting Randomness Using
+ *		Few Independent Sources" by Barak et. al. The algorithm takes
+ *		three samples s1, s2 and s3.  Samples s1 and s2 are multiplied
+ *		together (using modular multiplication) and the result is added
+ *		to s3.
+ *
+ * TOEPLITZ:	Apply the technique proposed in "True Random Number Generators
+ *		Secure in a Changing Environment" by Barak et. al.  Perform a
+ *		matrix multiplication of the Toeplitz matrix (i.e. diagonal
+ *		matrix) and the input data (in column form).
+ */
+enum {
+	NONE,
+	VON_NEUMANN,
+	SHA3,
+	MULTI_SAMPLE,
+	TOEPLITZ
+} pp_type;
+
+/*
+ * 
+ */
+typedef struct _pp_data {
+	//Type of post-processing
+	enum pp_type type;
+
+	//Input size + data
+	unsigned int input_size; //TODO do we need to set this to a fixed size?
+	union idata {
+		char* sample;
+		char* samples[3];
+	} input_data;
+
+	//Output size + data
+	unsigned int output_size;
+	char* output_data;
+} pp_data;
+
+/*
+ * Apply post-processing to the supplied data.  The calling function must set
+ * the type of post-processing and the input data on which to apply it.  On
+ * return, the output data will be filled with dispersed data.
+ *
+ * Arguments:
+ *	data:	Data struct (defined above) which specifies the type of
+ *		post-processing to apply and the data to apply it to.	
+ *
+ * Returns:
+ *	Success (0) if the data was successfully post-processed, or failure (1)
+ *	otherwise.
+ */
+int process(pp_data* data);
